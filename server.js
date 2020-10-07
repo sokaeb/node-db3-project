6ed6bded29a8f69e916daf27de71e0f35d39951b
Kae Benton <sonja.k.benton@gmail.com>
2020-10-07 16:28:48 -0400

    find and findById db methods working

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
-
+// const db = require('./data/db-config');
 const SchemeRouter = require('./schemes/scheme-router.js');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
+
+// server.get('/schemes', (req, res) => {
+//     db('schemes')
+//     .then(schemes => {
+//         res.status(200).json({ data: schemes })
+//     })
+//     .catch(err => {
+//         res.status(500).json({ message: err.message })
+//     })
+// })
 
 module.exports = server;
