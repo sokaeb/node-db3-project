660dab8ebec02208cbd98b4719cb6529a8566598
Kae Benton <sonja.k.benton@gmail.com>
2020-10-07 20:20:14 -0400

    all db methods working

--- schemes/scheme-router.js
+++ schemes/scheme-router.js
@@ -87,7 +87,7 @@ router.put('/:id', (req, res) => {
     if (scheme) {
       Schemes.update(changes, id)
       .then(updatedScheme => {
-        res.json(updatedScheme);
+        res.json({updated: updatedScheme});
       });
     } else {
       res.status(404).json({ message: 'Could not find scheme with given id' });
